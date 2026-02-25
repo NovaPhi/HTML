b45f2d4716e30e7f329e873cf226ae6e55d91619
NovaPhi <luisjaimeariass@gmail.com>
2026-02-25 14:24:38 -0600

    waaaaaaaaaaa

---
+++ Notes/19-feb-26/catCafe.js
@@ -0,0 +1,8 @@
+function traducir(){
+            document.getElementById("par1").innerHTML="Traduccion"
+        }
+
+function background(){
+    if(backgroundImage == )
+    document.style.backgroundImage= none;    
+}
